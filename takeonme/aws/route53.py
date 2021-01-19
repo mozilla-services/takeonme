e2180c4f5ba0c5743bc35a83535238e2f24de95d
Greg Guthe <gguthe@mozilla.com>
2021-01-19 11:21:57 -0500

    aws: fix iterating over zone and record sets

--- takeonme/aws/route53.py
+++ takeonme/aws/route53.py
@@ -13,20 +13,20 @@ def get_client() -> boto3.session.Session.client:
 def fetch_hosted_zones(
     client: boto3.session.Session.client,
 ) -> Generator[Dict[str, Any], None, None]:
-    for zone in client.get_paginator("list_hosted_zones").paginate():
-        assert "HostedZones" in zone
-        yield zone["HostedZones"]
+    for zones in client.get_paginator("list_hosted_zones").paginate():
+        assert "HostedZones" in zones
+        yield from zones["HostedZones"]
 
 
 def fetch_records_for_hosted_zone(
     client: boto3.session.Session.client, hosted_zone: Dict[str, Any]
 ) -> Generator[Dict[str, Any], None, None]:
     assert "Id" in hosted_zone
-    for record_set in client.get_paginator("list_resource_record_sets").paginate(
+    for record_sets in client.get_paginator("list_resource_record_sets").paginate(
         HostedZoneId=hosted_zone["Id"]
     ):
-        assert "ResourceRecordSets" in record_set
-        yield record_set["ResourceRecordSets"]
+        assert "ResourceRecordSets" in record_sets
+        yield from record_sets["ResourceRecordSets"]
 
 
 def get_all_records() -> List[Dict[str, Any]]:
